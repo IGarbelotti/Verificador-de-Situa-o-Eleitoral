68365c4b5b4b6532047de3857f5e1a36c2d40745
Isabela Garbelotti <99509870+IGarbelotti@users.noreply.github.com>
2022-05-02 14:54:29 -0300

    Add files via upload

--- formulário.js
+++ formulário.js
@@ -4,15 +4,12 @@ var r = document.getElementById("resultado")
 function verificar(){
     var iValor = i.value 
 
-    if(iValor>=18 && iValor<=70){
+    if(iValor>=18 && iValor<70){
         r.innerText = "Voce se encontra dentro da faixa etária requerida para votar."
     }
-    else if(iValor>=16){
+    else if(iValor>=16 || iValor>=70){
         r.innerText = "Voce se encontra em idade de voto facultativo."
 
-    }
-    else if(iValor>=70){
-        r.innerText = "Voce se encontra em idade de voto facultativo."
     }
     else{
         r.innerText = "Voce não se encontra em idade apta para votar."
